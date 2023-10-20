0be56700cc29df79ca269763c82d04031fd623f1
Ahmed <ahmeba@gmail.com>
2023-10-20 19:04:30 +0000

    Update app_constants.dart

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,5 +1,5 @@
 abstract class ThingsboardAppConstants {
-  static final thingsBoardApiEndpoint = 'http://localhost:8080';
+  static final thingsBoardApiEndpoint = 'http://things.sofia-networks.com:8080/';
   static final thingsboardOAuth2CallbackUrlScheme = 'org.thingsboard.app.auth';
 
   /// Not for production (only for debugging)
